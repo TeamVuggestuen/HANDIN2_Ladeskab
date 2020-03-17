442495512190a872a61b98ca23a68c8135c82e93
Marc Pramgaard Warming <au597165@uni.au.dk>
2020-03-17 21:37:25 +0100

    Test lavet

---
+++ Ladeskab.App/Ladeskab.Test/TestDoor.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Ladeskab.Test
+{
+    [TestFixture]
+    public class TestDoor
+    {
+        public Door _uut;
+        public EventArgs _receivedEventArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Door();
+
+
+            //Tjekker på events
+            _receivedEventArgs = null;
+
+            _uut.DoorEvent +=
+                (o, args) =>
+                {
+                    _receivedEventArgs = args;
+                };
+
+        }
+
+        [Test]
+        public void ctor_DoorIsUnLocked()
+        {
+            Assert.That(_uut.doorIsLocked, Is.False);
+        }
+
+        [Test]
+        public void ctor_DoorIsClosed()
+        {
+            Assert.That(_uut.doorIsClosed, Is.True);
+        }
+
+        [Test]
+        public void CheckOnDoorIsOpen()
+        {
+            _uut.OnDoorOpen();
+
+            Assert.That(_uut.doorIsLocked, Is.False);
+
+        }
+
+        [Test]
+        public void CheckOnDoorIsClose()
+        {
+            _uut.OnDoorClosed();
+            _uut.doorIsLocked = true;
+            Assert.That(_uut.doorIsLocked, Is.True);
+
+        }
+
+        //[Test]
+        //public void CheckOnDoorIsOpenEvent()
+        //{
+
+        //    _uut.OnDoorClosed();
+        //    Assert.That(_receivedEventArgs, Is.Ordered);
+        //}
+    }
+
+
+}
---
+++ Ladeskab.App/Ladeskab.Test/TestStationControl.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Ladeskab;
+using NSubstitute;
+
+namespace Ladeskab.Test
+{
+    [TestFixture]
+    public class TestStationControl
+    {
+        private StationControl _uut;
+        private IDoor _door;
+        private IDisplay _display;
+        private IRFIDReader _rfidReader;
+        private UsbChargerSimulator _usbChargerSimulator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _door = Substitute.For<IDoor>();
+            _display = Substitute.For<IDisplay>();
+            _rfidReader = Substitute.For<IRFIDReader>();
+            _usbChargerSimulator = Substitute.For<UsbChargerSimulator>();
+
+            _uut = new StationControl(_door, _display, _rfidReader, _usbChargerSimulator);
+        }
+
+        //[Test]
+        //public void Ladeskab_teststate()
+        //{
+        //    Assert.That(_uut. Is.False);
+        //}
+
+
+
+
+
+    }
+}
