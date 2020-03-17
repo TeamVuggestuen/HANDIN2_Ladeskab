89ef88ee258045d32c7137589102b01e8a5b3409
Brian Routhaues Boeg Stjernholm <au170668@uni.au.dk>
2020-03-17 23:07:02 +0100

    prøver lige program forfra

--- Ladeskab.App/Ladeskab.App/Program.cs
+++ Ladeskab.App/Ladeskab.App/Program.cs
@@ -3,63 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ladeskab;
 
-namespace Ladeskab
+namespace Ladeskab.App
 {
     class Program
     {
-        // SNAKKET MED FRANK OM HVORDAN DET DE KOM MED OVER, DOG KAN MAIN IKKE VÆRE STATIC, HVILKET HAN MENTE DEN SKULLE BLIVE VED MED AT VÆRE, MEN HAR FJERNET DET DA DET LAVEDE FEJL
-        public Door _door = new Door();
-        public Display _display = new Display();
-        public UsbChargerSimulator _usbChargerSimulator = new UsbChargerSimulator();
-        public RfidReader _rfidReader = new RfidReader();
-        private StationControl _stationControl;
-
-
-        //Static fjernet
-        void Main(string[] args)
+        static void Main(string[] args)
         {
-            // Assemble your system here from all the classes
 
-            //EN AF DE TING VI SKAL HA MED IND I VORES MAIN.
-            _stationControl = new StationControl(_door, _display, _rfidReader, _usbChargerSimulator);
-
-            bool finish = false;
-            do
-            {
-                string input;
-                System.Console.WriteLine("Indtast E, O, C, R: ");
-                input = Console.ReadLine();
-                if (string.IsNullOrEmpty(input)) continue;
-
-                switch (input[0])
-                {
-                    case 'E':
-                        finish = true;
-                        break;
-
-                    case 'O':
-                        _door.OnDoorOpen();
-                        break;
-
-                    case 'C':
-                        _door.OnDoorClosed();
-                        break;
-
-                    case 'R':
-                        System.Console.WriteLine("Indtast RFID id: ");
-                        string idString = System.Console.ReadLine();
-
-                        int id = Convert.ToInt32(idString);
-                        //rfidReader.OnRfidRead(id); LAVER FEJL TIL DEN ER IMPLEMENTERET
-                        break;
-
-                    default:
-                        break;
-                }
-
-            } while (!finish);
         }
     }
 }
--- Ladeskab.App/Ladeskab.Core.Library/Interfaces/IDoor.cs
+++ Ladeskab.App/Ladeskab.Core.Library/Interfaces/IDoor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Ladeskab.App;
 using Ladeskab;
 
 namespace Ladeskab
