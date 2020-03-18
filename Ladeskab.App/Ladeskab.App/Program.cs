9eab1f051844ac005ef27707a7a3ad72341f8cd2
Brian Routhaues Boeg Stjernholm <au170668@uni.au.dk>
2020-03-18 10:03:58 +0100

    app/program opdateret

--- Ladeskab.App/Ladeskab.App/Program.cs
+++ Ladeskab.App/Ladeskab.App/Program.cs
@@ -1,16 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ladeskab.App
+namespace Ladeskab
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //_stationControl = new StationControl(_door, _display, _rfidReader, _usbChargerSimulator);
+            Door _door = new Door();
+            Display _display = new Display();
+            UsbChargerSimulator _charger = new UsbChargerSimulator();
+            RfidReader _rfidReader = new RfidReader();
 
+            StationControl _control = new StationControl(_door, _display, _rfidReader, _charger);
+
+            bool finish = false;
+
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        _door.OnDoorOpen();
+                        break;
+
+                    case 'C':
+                        _door.OnDoorClosed();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        //rfidReader.OnRfidRead(id); LAVER FEJL TIL DEN ER IMPLEMENTERET
+                        break;
+
+                    default:
+                        break;
+                }
+            } while (!finish);
         }
     }
 }
