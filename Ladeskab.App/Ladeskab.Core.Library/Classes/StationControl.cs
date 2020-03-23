646aff4edc893c92d58bcb01dce0b4dd4a9adafa
Brian Routhaues Boeg Stjernholm <au170668@uni.au.dk>
2020-03-24 00:18:08 +0100

    Added tests in Stationcontrol

--- Ladeskab.App/Ladeskab.Core.Library/Classes/StationControl.cs
+++ Ladeskab.App/Ladeskab.Core.Library/Classes/StationControl.cs
@@ -23,14 +23,24 @@ namespace Ladeskab
         public IChargeControl _chargeControl;
         public IDoor _Door;
         public IDisplay _Display;
+        
+        //private int _oldId { get; set; }
+        
         private int _oldId;
+
+        public int OldId
+        {
+            get { return _oldId; }
+            private set { _oldId = value; }
+        }
+
 
 
         private string logFile = "logfile.txt"; // Navnet på systemets log-fil
-        private IDoor door;
-        private IChargeControl chargeControl;
-        private IDisplay display;
-        private IRfidReader rfid;
+        //private IDoor door;
+        //private IChargeControl chargeControl;
+        //private IDisplay display;
+        //private IRfidReader rfid;
 
         public StationControl(IDoor Door, IDisplay display, IRfidReader RfidReader, IChargeControl chargeControl)
         {
@@ -47,13 +57,13 @@ namespace Ladeskab
             _state = LadeskabState.Available;
         }
 
-        public StationControl(IDoor door, IChargeControl chargeControl, IDisplay display, IRfidReader rfid)
-        {
-            this.door = door;
-            this.chargeControl = chargeControl;
-            this.display = display;
-            this.rfid = rfid;
-        }
+        //public StationControl(IDoor door, IChargeControl chargeControl, IDisplay display, IRfidReader rfid)
+        //{
+        //    this.door = door;
+        //    this.chargeControl = chargeControl;
+        //    this.display = display;
+        //    this.rfid = rfid;
+        //}
 
 
         private void HandleDoorEvent(object sender, DoorEventArgs e)
--- Ladeskab.App/Ladeskab.Test/TestStationControl.cs
+++ Ladeskab.App/Ladeskab.Test/TestStationControl.cs
@@ -12,43 +12,96 @@ using NSubstitute;
 
 namespace Ladeskab.Test
 {
-    //[TestFixture]
-    //public class TestStationControl
-    //{
+    [TestFixture]
+    public class TestStationControl
+    {
 
-    //    private StationControl _uut;
-    //    private IDoor _door;
-    //    private IDisplay _display;
-    //    private IRfidReader _rfid;
-    //    private IChargeControl _chargeControl;
+        private StationControl _uut;
+        private IDoor _door;
+        private IDisplay _display;
+        private IRfidReader _rfid;
+        private IChargeControl _chargeControl;
 
-    //    [SetUp]
-    //    public void Setup()
-    //    {
+        [SetUp]
+        public void Setup()
+        {
 
-    //        _door = Substitute.For<IDoor>();
-    //        _display = Substitute.For<IDisplay>();
-    //        _rfid = Substitute.For<IRfidReader>();
-    //        _chargeControl = Substitute.For<IChargeControl>();
+            _door = Substitute.For<IDoor>();
+            _display = Substitute.For<IDisplay>();
+            _rfid = Substitute.For<IRfidReader>();
+            _chargeControl = Substitute.For<IChargeControl>();
 
-    //        _uut = new StationControl(_door, _chargeControl, _display, _rfid);
-
-    //    }
-
-    //    [Test]
-    //    public void TestRfidDetected_Available_testDoorlocking()
-    //    {
-    //        _chargeControl.isConnected().Returns(true);
-
-    //        _rfid.RfidEvent += Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
-
-    //        _door.Received().LockDoor();
-
-    //        Assert.That(  Is.EqualTo(1234));
-    //    }
+            _uut = new StationControl(_door, _display, _rfid, _chargeControl);
+        }
 
 
+        [Test]
+        public void TestRfidDetected_Available_checkLockDoor()
+        {
+            _chargeControl.isConnected().Returns(true);
+
+            _rfid.RfidEvent += Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
+
+            _door.Received().LockDoor(); // spørg Frank i morgen
+        }
 
 
-    //}
+        [Test]
+        public void TestRfidDetected_Available_checkStartCharge()
+        {
+            _chargeControl.isConnected().Returns(true);
+
+            _rfid.RfidEvent += Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
+
+            _chargeControl.Received().StartCharge();
+        }
+
+
+        [Test]
+        public void TestRfidDetected_Locked_testStopCharging()
+        {
+            //Act
+            _chargeControl.isConnected().Returns(true);
+
+            _rfid.RfidEvent +=
+                Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
+            _rfid.RfidEvent +=
+                Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
+
+                //Assert
+            _chargeControl.Received().StopCharge();
+        }
+
+
+        [Test]
+        public void TestRfidDetected_Available_checkOldId()
+        {
+            //Assert
+            _chargeControl.isConnected().Returns(true);
+
+            //Act
+            _rfid.RfidEvent +=
+                Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
+
+            //Assert
+            Assert.That(_uut.OldId, Is.EqualTo(1234));
+        }
+
+        [Test]
+        public void TestRfidDetected_Locked_checkOldId()
+        {
+            //Assert
+            _chargeControl.isConnected().Returns(true);
+
+            //Act
+            _rfid.RfidEvent +=
+                Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
+            _rfid.RfidEvent +=
+                Raise.EventWith(new RfidEventArgs() { Rfid_ID = 1234 });
+
+            //Assert
+            Assert.That(_uut.OldId, Is.EqualTo(1234));
+        }
+
+    }
 }
